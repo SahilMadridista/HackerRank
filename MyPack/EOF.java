e46768d08389a8f6cb8150bf082487ad50c13b3f
sahil <sahil91098singh@gmail.com>
2020-04-23 09:37:45 +0530

    End-of-file problem

---
+++ MyPack/EOF.java
@@ -0,0 +1,14 @@
+package MyPack;
+
+public class EOF {
+
+	public EOF() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
